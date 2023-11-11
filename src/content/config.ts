fd93d71adac3257d869d925d92f891993fc52422
Bruno <69008445+BrunoRoemers@users.noreply.github.com>
2023-11-11 16:08:30 +0100

    Add boilerplate for project content type

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,5 +1,12 @@
 import { z, defineCollection } from "astro:content";
 
+const projects = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+  }),
+});
+
 const posts = defineCollection({
   type: "content",
   schema: z.object({
@@ -8,5 +15,6 @@ const posts = defineCollection({
 });
 
 export const collections = {
+  projects: projects,
   posts: posts,
 };
